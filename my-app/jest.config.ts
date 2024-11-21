2c332d1fa7c7c8057399c93e3e0d46e0db99ba95
cloud0406 <cloud0406@naver.com>
2024-11-21 15:06:21 +0900

    test

--- my-app/jest.config.ts
+++ my-app/jest.config.ts
@@ -12,6 +12,9 @@ const config: Config = {
   testEnvironment: "jsdom",
   // Add more setup options before each test is run
   // setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  collectCoverage: true, // 커버리지 리포트 생성 활성화
+  coverageDirectory: "coverage", // 커버리지 리포트 저장 폴더
+  coverageReporters: ["json", "lcov", "text", "clover"], // 리포트 포맷 설정
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
